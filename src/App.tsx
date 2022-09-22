1ddbabe23fb281ca87c78007a957888bef637fb8
Alejandro Reyna <AleejandroReyna@gmail.com>
2022-09-22 00:21:11 -0500

    custom-base: Added cytoscape (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+
+import CytoscapeComponent from 'react-cytoscapejs';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const elements = [
+    { data: { id: 'one', label: 'Node 1' }, position: { x: 100, y: 100 } },
+    { data: { id: 'two', label: 'Node 2' }, position: { x: 200, y: 200 } },
+    { data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2' } }
+  ];
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <h1>React Cytoscape Linked List</h1>
+
+      <CytoscapeComponent elements={elements} style={ { width: '1000px', height: '1000px' } } />
+
     </div>
   )
 }
